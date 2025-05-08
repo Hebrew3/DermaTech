610149d307e9a859e4c53251ec485d65f29484b3
Hebrew3 <ddeasis1990@gmail.com>
2025-05-08 12:39:38 +0800

    Dermatech UI

--- DermaTech/src/App.jsx
+++ DermaTech/src/App.jsx
@@ -1,7 +1,18 @@
-import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from './Login';
+import Schedule from './Schedule';
+import Home from './Home'; // dummy component
 
 function App() {
   return (
-    <div classname></div>
-  )
+    <BrowserRouter>
+    tgtytg
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
+
+export default App;
